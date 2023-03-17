cf29e1c725aebf605510bea5dbc110a0d1e40d3f
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2023-03-17 00:04:24 -0700

    Create script to setup k3s until argocd is functional

--- k3-system-setup.sh
+++ k3-system-setup.sh
@@ -15,3 +15,7 @@ helm install prometheus prometheus-community/kube-prometheus-stack -n prometheus
 kubectl create namespace nginx-ingress
 helm repo add nginx-stable https://helm.nginx.com/stable
 helm install nginx-ingress nginx-stable/nginx-ingress --set rbac.create=true -n nginx-ingress
+
+kubectl create namespace cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm install cert-manager jetstack/cert-manager --set installCRDs=true -n cert-manager
