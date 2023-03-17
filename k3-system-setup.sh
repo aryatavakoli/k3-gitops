33f736458b083e32c5e4b40d48ba03793f0fde8d
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2023-03-16 23:59:53 -0700

    Create script to setup k3s until argocd is functional

---
+++ k3-system-setup.sh
@@ -0,0 +1,17 @@
+kubectl create namespace tigera-operator
+helm repo add projectcalico https://docs.tigera.io/calico/charts
+helm install calico projectcalico/tigera-operator -f argo/applicationsets/git-generator-directory/apps/tigera-operator/values.yaml --namespace tigera-operator
+
+kubectl create namespace metallb-system
+helm repo add metallb https://metallb.github.io/metallb
+helm install metallb metallb/metallb -n metallb-system
+sleep 2m
+kubectl apply -f argo/applicationsets/git-generator-directory/apps/metallb/config.yaml -n metallb-system
+
+kubectl create namespace prometheus
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install prometheus prometheus-community/kube-prometheus-stack -n prometheus
+
+kubectl create namespace nginx-ingress
+helm repo add nginx-stable https://helm.nginx.com/stable
+helm install nginx-ingress nginx-stable/nginx-ingress --set rbac.create=true -n nginx-ingress
